3e7d3c9dd74c0d84e7367e4be70580fbbd379904
Musa Jahun <msjahun@live.com>
2018-05-16 15:05:35 +0300

    populated the spinners and I also added two new activities, login activity and login success Activity

--- app/src/main/java/com/example/dc/spinnerapplication/MainActivity.java
+++ app/src/main/java/com/example/dc/spinnerapplication/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.dc.spinnerapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinnerMain, spinnerCountryCode;
+    ArrayAdapter adapter;
+    String [] items = {"Female", "Male"};
+    String [] countryCode = {"+90", "+011", "+234", "+55", "+447"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinnerMain = (Spinner) findViewById(R.id.spinnerMain);
+        spinnerCountryCode = (Spinner) findViewById(R.id.spinnerCountryCode);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMain.setAdapter(adapter);
+
+        adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, countryCode);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCountryCode.setAdapter(adapter);
+
+
     }
 }
